6c146ecff0985c3426570ea53e0d082a0db7f809
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-02 12:39:47 +0530

    basic controller functionality

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,24 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.response.DataResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/tdd/")
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloWorldController {
 
-    @Autowired
-    private Translator englishTranslator;
-
-    @GetMapping("helloworld")
-    public DataResponse greetHello(
-            @RequestParam(name = "name", defaultValue = "World", required = false)String name){
-        return new DataResponse("Hello "+name+"!");
+    @RequestMapping("/")
+    public String getDefaultPage(){
+        return "hello";
     }
 
-    @GetMapping("{locale}/helloworld")
-    public DataResponse greetHelloLocaleSpecific(@PathVariable(name = "locale") String locale){
-        return new DataResponse(englishTranslator.getTranslatedValue("Hello World!",locale));
-    }
 }
--- src/main/java/com/example/demo/Translator.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-public interface Translator {
-
-    String getTranslatedValue(String text, String locale);
-}
