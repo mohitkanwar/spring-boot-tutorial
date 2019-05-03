0cd58235dff662c563367316bf389727eecf9204
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-03 10:05:39 +0530

    Intro to thymeleaf

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path = "/mycontroller")
 public class HelloWorldController {
 
