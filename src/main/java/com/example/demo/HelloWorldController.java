26c96eb8d893a08caaf441e1729c802886d66684
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-09 15:33:32 +0530

    added other ways of enriching the model

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -3,20 +3,42 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping(path = "/mycontroller")
 public class HelloWorldController {
 
     @RequestMapping(path = "/defaultpage" ,method = RequestMethod.GET)
-    //@ResponseBody
     public String getDefaultPage(Model model){
         model.addAttribute("attribute","The Value of the attribute passed from controller");
         return "hello";
+    }
+
+    @RequestMapping(path = "/specialpage" ,method = RequestMethod.GET)
+    public ModelAndView getSpecailPage(){
+        ModelAndView modelAndView = new ModelAndView("special");
+        modelAndView.addObject("attribute","The Value of the attribute passed from ModelAndView");
+        return modelAndView;
+    }
+
+    /**
+     * Common Model Attributes injected in all views
+     * @return
+     */
+    @ModelAttribute("messages")
+    public List<String> messages() {
+        List<String> messages = new ArrayList<>();
+        messages.add("This is first Message");
+        messages.add("This is Second Message");
+        return messages;
     }
 
 }
