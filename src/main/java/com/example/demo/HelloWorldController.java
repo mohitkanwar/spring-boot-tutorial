c7a023ba4ed23d89bea19a22835f7ad1157ea7fe
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-09 14:53:27 +0530

    mvc

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +14,8 @@ public class HelloWorldController {
 
     @RequestMapping(path = "/defaultpage" ,method = RequestMethod.GET)
     //@ResponseBody
-    public String getDefaultPage(){
+    public String getDefaultPage(Model model){
+        model.addAttribute("attribute","The Value of the attribute passed from controller");
         return "hello";
     }
 
