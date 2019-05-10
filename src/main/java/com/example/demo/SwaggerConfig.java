82bae68e60a45bc7ac9177cf86425b05069efc8e
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-10 12:24:09 +0530

    added API Info

--- src/main/java/com/example/demo/SwaggerConfig.java
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -13,11 +18,23 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket productApi() {
+    public Docket swaggerDocket() {
+        System.out.println(this.getClass().getPackage().getName());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .apis(RequestHandlerSelectors.basePackage(this.getClass().getPackage().getName()))
                 .paths(regex(".*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Swagger Integration with Spring Boot",
+                "This is an example for integrating swagger with Spring boot for documentation.",
+                "0.1",
+                "Terms of service",
+                new Contact("Mohit Kanwar", "www.mohitkanwar.com", "m.m.kanwar@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
