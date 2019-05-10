9cff9c9ec750e3da62a62207415b4f2f6ff61e72
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-10 10:34:51 +0530

    configuring swagger

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -10,7 +10,6 @@ public class HelloWorldController {
 
     //http://localhost:8080/mycontroller/english/defaultpage?name=Dunia
     @RequestMapping(path = "/{locale}/defaultpage" ,method = RequestMethod.GET)
-    //@ResponseBody
     public String getDefaultPage(@RequestParam(name = "name",required = false, defaultValue = "World") String name,
                                  @PathVariable(name = "locale") String locale){
         return "hello " +name+". The chosen locale is "+locale;
